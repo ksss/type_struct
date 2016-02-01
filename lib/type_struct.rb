d853d7c606a75b2e33d18abcbb247ecaddbe66cb
ksss <co000ri@gmail.com>
2016-02-01 23:05:35 +0900

    Move basic methods

--- lib/type_struct.rb
+++ lib/type_struct.rb
@@ -4,7 +4,77 @@ class TypeStruct
   class NoMemberError < StandardError
   end
 
+  def initialize(**arg)
+    self.class.members.each do |k, _|
+      self[k] = arg[k]
+    end
+  end
+
+  def ==(other)
+    return false unless TypeStruct === other
+    return false unless to_h == other.to_h
+    true
+  end
+
+  def []=(k, v)
+    __send__("#{k}=", v)
+  end
+
+  def [](k)
+    __send__(k)
+  end
+
+  def inspect
+    m = to_h.map do |k, v|
+      "#{k}=#{v.inspect}"
+    end
+    "#<#{self.class} #{m.join(', ')}>"
+  end
+
+  def to_h
+    m = {}
+    self.class.members.each do |k, _|
+      m[k] = self[k]
+    end
+    m
+  end
+
   class << self
+    def from_hash(h)
+      h.map { |k, v|
+        if members[k].ancestors.include?(TypeStruct)
+          new(k => members[k].new(v))
+        else
+          new k => v
+        end
+      }.first
+    end
+
+    def members
+      const_get(:MEMBERS)
+    end
+
+    def type(k)
+      members[k]
+    end
+
+    def valid?(k, v)
+      t = type(k)
+      unless Hash === t
+        t = { type: t, nilable: false }
+      end
+      if t[:nilable] == true && v.nil?
+        true
+      elsif Array === t[:type]
+        return false if v.nil?
+        v.all? { |i| t[:type].any? { |c| c === i } }
+      elsif TypeStruct === v
+        t[:type] == v.class
+      else
+        t[:type] === v
+      end
+    end
+
     alias original_new new
     def new(**args)
       Class.new(TypeStruct) do
@@ -13,41 +83,6 @@ class TypeStruct
         class << self
           def new(*args)
             original_new(*args)
-          end
-
-          def from_hash(h)
-            h.map { |k, v|
-              if members[k].ancestors.include?(TypeStruct)
-                new(k => members[k].new(v))
-              else
-                new k => v
-              end
-            }.first
-          end
-
-          def members
-            const_get(:MEMBERS)
-          end
-
-          def type(k)
-            members[k]
-          end
-
-          def valid?(k, v)
-            t = type(k)
-            unless Hash === t
-              t = { type: t, nilable: false }
-            end
-            if t[:nilable] == true && v.nil?
-              true
-            elsif Array === t[:type]
-              return false if v.nil?
-              v.all? { |i| t[:type].any? { |c| c === i } }
-            elsif TypeStruct === v
-              t[:type] == v.class
-            else
-              t[:type] === v
-            end
           end
         end
 
@@ -63,41 +98,6 @@ class TypeStruct
             end
             instance_variable_set("@#{k}", v)
           end
-        end
-
-        def initialize(**arg)
-          self.class.members.each do |k, _|
-            self[k] = arg[k]
-          end
-        end
-
-        def ==(other)
-          return false unless TypeStruct === other
-          return false unless to_h == other.to_h
-          true
-        end
-
-        def []=(k, v)
-          __send__("#{k}=", v)
-        end
-
-        def [](k)
-          __send__(k)
-        end
-
-        def inspect
-          m = to_h.map do |k, v|
-            "#{k}=#{v.inspect}"
-          end
-          "#<#{self.class.to_s} #{m.join(', ')}>"
-        end
-
-        def to_h
-          m = {}
-          self.class.members.each do |k, _|
-            m[k] = self[k]
-          end
-          m
         end
       end
     end
