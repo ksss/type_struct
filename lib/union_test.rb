c03e20f06ea5ed3031ccb63ba0b3a23f09d134b0
ksss <co000ri@gmail.com>
2016-02-04 17:56:45 +0900

    Add Union class test

---
+++ lib/union_test.rb
@@ -0,0 +1,45 @@
+require "type_struct"
+
+module UnionTest
+  U = Union.new(TrueClass, FalseClass)
+  def test_union(t)
+    unless Union === U
+      t.error("union error")
+    end
+  end
+
+  def test_or(t)
+    if U === nil
+      t.error("nil")
+    end
+
+    n = U | NilClass
+    if U === nil
+      t.error("nil")
+    end
+    unless n === nil
+      t.error("nil")
+    end
+  end
+
+  def test_equal(t)
+    unless U === true
+      t.error("union error")
+    end
+    unless U === false
+      t.error("union error")
+    end
+    if U === nil
+      t.error("union error")
+    end
+  end
+
+  using UnionExt
+
+  def test_class_or(t)
+    u = TrueClass | FalseClass
+    unless u === true
+      t.error("error")
+    end
+  end
+end
