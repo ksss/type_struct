0c73d5add085a68a410ea691e446cd966ed5e8c4
ksss <co000ri@gmail.com>
2016-03-12 17:48:15 +0900

    To short name

--- lib/type_struct/ext.rb
+++ lib/type_struct/ext.rb
@@ -19,6 +19,18 @@ class TypeStruct
       "#{self.class.name.split('::').last}(#{@key_type}, #{@value_type})"
     end
   end
+
+  class Interface
+    def to_s
+      "#<#{self.class.name.split('::').last}(#{@methods.map(&:inspect).join(',')})>"
+    end
+  end
+
+  class Union
+    def to_s
+      "#<#{self.class.name.split('::').last} #{@classes.join('|')}>"
+    end
+  end
 end
 
 module Kernel
