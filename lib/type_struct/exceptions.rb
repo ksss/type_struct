2142cb6bb6da74debb97301c7972617707ae22c0
ksss <co000ri@gmail.com>
2016-07-16 09:54:37 +0900

    Fix for REPL

--- lib/type_struct/exceptions.rb
+++ lib/type_struct/exceptions.rb
@@ -17,7 +17,12 @@ class TypeStruct
         b = e.backtrace_locations.find do |b|
           b.absolute_path !~ THIS_LIB_REGEXP
         end
-        relative_path = Pathname.new(b.absolute_path).relative_path_from(PWD)
+        pathname = Pathname.new(b.absolute_path)
+        relative_path = if pathname.absolute?
+          pathname.relative_path_from(PWD)
+        else
+          b.absolute_path
+        end
         "#{relative_path}:#{b.lineno}:in #{e.class} #{e}"
       }.join("\n")
     end
